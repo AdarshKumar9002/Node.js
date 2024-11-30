85c36eea281d581cb3034d49fc7988a4286d3840
Adarsh <adarashkumar9002@gmail.com>
2024-11-30 19:35:53 +0530

    Removed the adminData import from shop.js

--- 07 - Model View Controller/routes/shop.js
+++ 07 - Model View Controller/routes/shop.js
@@ -4,8 +4,6 @@ const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-const adminData = require("./admin");
-
 router.get("/", productsController.getProducts);
 
 module.exports = router;
